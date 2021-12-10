4004abf9093da8502127d493f95457813e790c2a
zhangzhiming <zhangzhiming>
2021-12-11 00:04:49 +0800

    feat:add vue

--- app/http/router.go
+++ app/http/router.go
@@ -3,10 +3,13 @@ package http
 import (
 	"github.com/arms/app/http/module/demo"
 	"github.com/arms/framework/gin"
+	"github.com/arms/framework/middleware/static"
 )
 
 func Routes(r *gin.Engine) {
 
-	r.Static("/dist/", "./dist/")
+	// /路径先去./dist目录下查找文件是否存在，找到使用文件服务提供服务
+	//TODO:原理
+	r.Use(static.Serve("/", static.LocalFile("./vue/dist/", false)))
 	demo.Register(r)
 }
